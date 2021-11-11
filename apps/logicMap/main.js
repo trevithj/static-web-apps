cf41d1a607c7deaea30cedc9935d672469f0fae1
John Trevithick <john.trevithick@thldigital.com>
2021-11-11 19:16:45 +1300

    logicMap new direction. Initial changes.

--- apps/logicMap/main.js
+++ apps/logicMap/main.js
@@ -1,8 +1,14 @@
 (function() {
+	const { menu } = BASE.value('Selectors');
+	const theMenu = document.querySelector(menu);
 
+	theMenu.querySelectorAll('button').forEach(btn => {
+		btn.addEventListener('click', () => BASE.dispatch('BTN_CLICKED', btn));
+	})
 	//basic pattern
 	BASE.listen("SOMETHING_HAPPENED", function(data) {
 		BASE.dispatch("DO_SOMETHING", {});
 	});
+
 
 }());
