b94b2b4b43a51196542110e2a56cde2b3f917599
John Trevithick <trevithj@gmail.com>
2022-06-22 08:01:45 +1200

    PubSub updates

--- common/toaster.js
+++ common/toaster.js
@@ -12,11 +12,11 @@ window.addEventListener('load', e => {
     document.body.appendChild(wrapper);
 
     const toaster = wrapper.querySelector('div#actual-toaster');
-    wrapper.style.display = 'none';
+    toaster.style.display = "none";
 
     function hideToaster() {
         //TODO: trigger animation of some sort?
-        wrapper.style.display = 'none';
+        toaster.style.display = 'none';
         toaster.classList.remove('error', 'warn', 'info');
     }
 
@@ -36,7 +36,7 @@ window.addEventListener('load', e => {
         toaster.classList.add(type);
 
         toaster.innerText = msg; 
-        wrapper.style.display = '';
+        toaster.style.display = '';
     }
 
     window.PubSub.subscribe('ShowToaster', subscriber);
